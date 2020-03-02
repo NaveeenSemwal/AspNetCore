cb046ee2701b96662612f18f1e769f05b9cb67a1
NaveeenSemwal <semwal.dotnet@gmail.com>
2020-03-02 14:50:18 +0530

    Added Microsoft.AspNetCore.Mvc.NewtonsoftJson

--- Installers/MvcInstaller.cs
+++ Installers/MvcInstaller.cs
@@ -14,9 +14,10 @@ namespace TweetBook.Installers
                 options.SwaggerDoc("v1", new OpenApiInfo { Title = "Tweetbook API", Version = "v1" });
             });
 
-
+            //  Issue :  System.Text.Json.JsonException: A possible object cycle was detected which is not supported. 
+            // This can either be due to a cycle or if the object depth is larger than the maximum allowed depth of 32.
             services.AddControllers().AddNewtonsoftJson(options =>
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+          options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
     }
 }
